f0b1d123c5617ccd9a9c3dbd92196a2e1906bc77
LLdaniel <daniel.adlkofer@freenet.de>
2025-03-21 17:16:03 +0100

    more robust handling of trains leaving the control center

--- Statistics.py
+++ Statistics.py
@@ -34,15 +34,22 @@ class Statistics:
         # update new delay
         for i in self.trains.copy():
             traindetailsxml = stsCon.send_message(stsCon.write_xml('zugdetails', [('zid', str(i.tid))]), 'zugdetails')
-            newdelay = int(stsCon.read_xml_single(traindetailsxml, 'zugdetails', 'verspaetung'))
-            self.add_delay(i.traintype, i.diff(newdelay))
-            
-            # remove trains that have become invisible
-            newvisible = False
-            if stsCon.read_xml_single(traindetailsxml, 'zugdetails', 'sichtbar') == 'false':
+            # handle case: train leaves control center, but is still in train list
+            if '<status code=\'402\' >' in traindetailsxml:
+                logger.warning('Train ID has become unknown (status code 402), train will be removed from list!')
+                logger.warning('Captured response: ' + traindetailsxml)
                 self.trains.remove(i)
-                #i.visible = False
-                #self.trainsInvisible.add(i) train completely gone? otherwise will be re-introduced with updated invisible listx
+            # handle case: train still present, calculate new delay
+            else:
+                newdelay = int(stsCon.read_xml_single(traindetailsxml, 'zugdetails', 'verspaetung'))
+                self.add_delay(i.traintype, i.diff(newdelay))
+
+                # remove trains that have become invisible
+                newvisible = False
+                if stsCon.read_xml_single(traindetailsxml, 'zugdetails', 'sichtbar') == 'false':
+                    self.trains.remove(i)
+                    #i.visible = False
+                    #self.trainsInvisible.add(i) train completely gone? otherwise will be re-introduced with updated invisible list
                 
         # add newly visible to list and remove from invisible list
         for j in self.trainsInvisible.copy():
