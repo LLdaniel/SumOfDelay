1f5a572e42db4f4c331c34132a119e56292a21b4
LLdaniel <daniel.adlkofer@freenet.de>
2025-03-22 21:13:46 +0100

    added new train types DE

--- TrainType.py
+++ TrainType.py
@@ -41,10 +41,10 @@ class TrainType:
             case 'DE':
                 self.freight = ('CB', 'CbZ', 'CFA', 'CFN', 'CHL', 'CIL', 'CL', 'CS', 'CSQ', 'CT', 'DFG', 'DGE', 'DGN', 'DGS', 'DGX', 'DGZ', 'DNG', 'EnKo', 'EUC', 'ExC', 'EZ', 'EZK', 'FE', 'FIR', 'FR', 'FS', 'FX', 'FZ', 'FZT', 'GAG', 'GAGC', 'GC', 'GX', 'GZ', 'HGK', 'ICG', 'ICL', 'IKE', 'IKL', 'IKS', 'IRC', 'KC', 'KCL', 'KT', 'NG', 'PIC', 'RA', 'RWE', 'TC', 'TEC', 'Tfzl', 'TKC', 'TRC')
                 self.longdistance = ('AS', 'AZ', 'CNL', 'D', 'DLr', 'DLt', 'DPE', 'DPF', 'EC', 'EN', 'FLX', 'IC', 'ICE', 'LNF', 'LPF', 'LPFT', 'LR', 'LRV', 'LRZ', 'NJ', 'RJ', 'RJX', 'TGV', 'THA', 'VX', 'WEST', 'X')
-                self.regional = ('ABR', 'AKN', 'ALX', 'BOB', 'CAN', 'DNR', 'EIB', 'ERB', 'EVB', 'FEG', 'FEX', 'HEX', 'HLB', 'HTB', 'HZL', 'IRE', 'LT', 'ME', 'MEr', 'MR', 'MRB', 'NBE', 'NEB', 'NEG', 'NOB', 'NWB', 'OE', 'OLA', 'PEG', 'PRE', 'RB', 'RbZ', 'RE', 'RTB', 'SBB', 'SHB', 'STB', 'SWE', 'UBB', 'VBG', 'VEC', 'VIA', 'WEG', 'WFB')
+                self.regional = ('ABR', 'AKN', 'ALX', 'BOB', 'CAN', 'DNR', 'EIB', 'ERB', 'EVB', 'FEG', 'FEX', 'HEX', 'HLB', 'HTB', 'HZL', 'IRE', 'LT', 'ME', 'MEr', 'MR', 'MRB', 'NBE', 'NEB', 'NEG', 'NOB', 'NWB', 'OE', 'OLA', 'PEG', 'PRE', 'RB', 'RbZ', 'RE', 'RTB', 'SBB', 'SHB', 'STB', 'SWE', 'UBB', 'VBG', 'VEC', 'VIA', 'WEG', 'WFB', 'BRB')
                 self.city = ('BSB', 'CBC', 'Dsts', 'B', 'G', 'H', 'I', 'P', 'W', 'E', 'LS', 'OSB', 'RT', 'S', 'Messeshuttle')
                 self.special = ('DbZ', 'DZ', 'M', 'MCT', 'PbZ', 'SDZ')
-                self.loco = ('L', 'Lok', 'LOKF', 'LZ', 'Rf', 'Schadl', 'C', 'F', 'R', 'Schadt', 'Schadw', 'T', 'Tfzf')
+                self.loco = ('L', 'Lok', 'LOKF', 'LZ', 'Rf', 'Schadl', 'C', 'F', 'R', 'Schadt', 'Schadw', 'T', 'Tfzf', 'Ersatzlok')
                 self.construction = ('Bauz', 'DGV')
                 try:
                     self.prefix = name[0:name.index(' ')]
